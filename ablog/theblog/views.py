51a00c58bd9c1bbaf41062672ed3be15bacf782d
Randy <Randyar79@github.co>
2020-10-30 20:34:40 +0530

    LIke/unlike
    Added a unlike option for liked post with loop concept

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -15,7 +15,15 @@ def LikeView(request, pk):
     # post = get_object_or_404(Post, id=request.POST.get('post_id'))
     post = Post.objects.get(id=pk)
     ''' as our site is not supporting for get_object_or_404 we assign the value directly in above line for post '''
-    post.likes.add(request.user)
+
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = True
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
@@ -40,8 +48,14 @@ class ArticleDetailView(DetailView):
         context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id):
+            liked = True
+
         context["cat_menu"] = cat_menu
         context["total_likes"] = total_likes
+        context["liked"] = liked
         return context
 
 
