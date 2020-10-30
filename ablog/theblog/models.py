b4837308d9a0c019157faffdc6faeb2c061fdb9e
Randy <Randyar79@github.co>
2020-10-30 23:06:44 +0530

    Comment Intial stage
    This time we added a comment section to see comment in a post form article details. need tmore to set in next phase

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -37,3 +37,13 @@ class Post(models.Model):
     def get_absolute_url():
         # return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    body = models.TextField(max_length=1000)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
